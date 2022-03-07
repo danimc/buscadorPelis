7ad82ec683579f609675bfa6f152e2fc3ba544df
OAG HQ <daniel_k310a@hotmail.com>
2022-03-07 17:30:23 -0600

    intentando react routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react"
+import { Routes, Route } from 'react-router-dom'
+
 import "./App.css"
 import "bulma/css/bulma.css"
 import { Detail } from "./pages/Detail"
@@ -7,14 +9,12 @@ import { Home } from "./pages/Home"
 class App extends Component {
 
   render() {
-    const url = new URL(document.location)
-    const Page = url.searchParams.has('id')
-      ? <Detail id={url.searchParams.get('id')} />
-      : <Home />
-
     return (
       <div className="App">
-        {Page}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="detail/:id" element={<Detail />} />
+        </Routes>
       </div>
     );
   }
